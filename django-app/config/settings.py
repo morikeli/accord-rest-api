b359c77c81ff773465fa65b1ac5f22131bd740a6
Mori Keli <78599959+morikeli@users.noreply.github.com>
2026-09-16 13:41:01 +0300

    feat: update INSTALLED_APPS to remove 'apps.' prefix from 'acord_intake'

--- django-app/config/settings.py
+++ django-app/config/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "apps.acord_intake",
+    "acord_intake",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
