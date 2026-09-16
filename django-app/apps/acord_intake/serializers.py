d4f7aead26efd6599f3ceddeee0e8680e24b3009
Mori Keli <78599959+morikeli@users.noreply.github.com>
2026-09-16 18:42:04 +0300

    feat: add serializer to handle health check messages

--- django-app/apps/acord_intake/serializers.py
+++ django-app/apps/acord_intake/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 
 from .models import ApsIncoming
 
+
+class HealthCheckSerializer(serializers.Serializer):
+    message = serializers.CharField()
+
+
 class AcordJSONIntakeSerializer(serializers.ModelSerializer):
     source = serializers.CharField(read_only=True)
 
