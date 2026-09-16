c1ad56787ff3977ba2f792b940004ee713dcaa15
Mori Keli <78599959+morikeli@users.noreply.github.com>
2026-09-16 22:36:05 +0300

    feat: expose endpoint for signup view

--- django-app/apps/acord_intake/urls.py
+++ django-app/apps/acord_intake/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("health", views.HealthCheckView.as_view()),
+    path("auth/signup", views.SignupView.as_view()),
     path("intake/json", views.IntakeAPIView.as_view()),
     path("aps/all", views.APSRecordsViewSet.as_view({"get": "list"})),
     path(
