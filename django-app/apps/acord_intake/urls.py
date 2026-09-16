f98dea1c36c38a5408502714a36e0af95049bb5f
Mori Keli <78599959+morikeli@users.noreply.github.com>
2026-09-16 21:41:55 +0300

    feat: add endpoints for APSRecords view set

--- django-app/apps/acord_intake/urls.py
+++ django-app/apps/acord_intake/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path("health", views.HealthCheckView.as_view()),
     path("intake/json", views.IntakeAPIView.as_view()),
-    
+    path("aps/all", views.APSRecordsViewSet.as_view({"get": "list"})),
+    path(
+        "aps/<int:pk>",
+        views.APSRecordsViewSet.as_view(
+            {"get": "retrieve", "put": "update", "delete": "destroy"}
+        ),
+    ),
 ]
