4deca01550fc2663e8f45e99d6c5f23731df7d9c
Mori Keli <78599959+morikeli@users.noreply.github.com>
2026-09-16 13:50:00 +0300

    feat: implement HealthCheck API view for application status

--- django-app/apps/acord_intake/views.py
+++ django-app/apps/acord_intake/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class HealthCheck(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {"message": "OK! App ran successfully!"}
+        return Response(data, status.HTTP_200_OK)
