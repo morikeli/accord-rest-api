16df7d72de23933d8e6e0982869fcf99589b9f77
Mori Keli <78599959+morikeli@users.noreply.github.com>
2026-09-16 22:37:14 +0300

    feat: add default permission and schema class to REST framework settings

--- django-app/config/settings.py
+++ django-app/config/settings.py
@@ -77,7 +77,11 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ]
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 SIMPLE_JWT = {
