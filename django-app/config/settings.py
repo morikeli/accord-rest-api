7f8adfa54461e27bab58eabded441fbb7ce742d0
Mori Keli <78599959+morikeli@users.noreply.github.com>
2026-09-16 22:46:16 +0300

    refactor: 🚮 remove basic and session authentication from REST framework settings

--- django-app/config/settings.py
+++ django-app/config/settings.py
@@ -74,8 +74,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.BasicAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
