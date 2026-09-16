758d3844693ed0d47bf3844ad05017349cfbdbcb
Mori Keli <78599959+morikeli@users.noreply.github.com>
2026-09-16 19:15:57 +0300

    feat: expose endpoints for documentation - Swagger and Redoc

--- django-app/config/urls.py
+++ django-app/config/urls.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
-    path('api/', include('acord_intake.urls')),
-    path('admin/', admin.site.urls),
+    path("api/", include("acord_intake.urls")),
+    path("admin/", admin.site.urls),
+
+    # Schema generator
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # Swagger UI    
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    # Redoc UI
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
