e6bbd9c81a181661eda815cb43b0d40231919d7b
Mori Keli <78599959+morikeli@users.noreply.github.com>
2026-09-17 13:04:26 +0300

    refactor: update expiration time for refresh and access tokens

--- django-app/config/settings.py
+++ django-app/config/settings.py
@@ -83,8 +83,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
